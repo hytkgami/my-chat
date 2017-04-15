304600305fb73a29ff8d8e2962114df655fead36
hytkgami <gagagatrump@gmail.com>
2017-04-15 14:25:12 +0900

    add the chat-body component

---
+++ src/app/chat-body/chat-body.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-body',
+  templateUrl: './chat-body.component.html',
+  styleUrls: ['./chat-body.component.sass']
+})
+export class ChatBodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
