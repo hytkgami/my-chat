858e7d4a82c4bbed68b439006c1ff2f80a3f1678
hytkgami <gagagatrump@gmail.com>
2017-04-15 14:40:40 +0900

    add buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ChatBodyComponent } from './chat-body/chat-body.component';
+import { InputFormComponent } from './input-form/input-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ChatBodyComponent
+    ChatBodyComponent,
+    InputFormComponent
   ],
   imports: [
     BrowserModule,
